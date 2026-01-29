f2bccc107b4cdcd40836bd33c7dadeee2b29c48a
attthulll <athuljayaraj15@gmail.com>
2026-01-29 19:31:23 +0530

    update

--- server.js
+++ server.js
@@ -17,7 +17,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    process.env.CLIENT_URL
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
